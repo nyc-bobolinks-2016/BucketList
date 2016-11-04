7a3cc968385130ce30036eef5f99a963b7d10836
Olivia Coffaro <email": null>
2016-11-04 11:04:50 -0400

    new and create methods in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
   def create
-    
+    @user = User.new(user_params)
+    @user.save
+    redireect_to '/'
+    # change to 'sessions/new'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
 end
