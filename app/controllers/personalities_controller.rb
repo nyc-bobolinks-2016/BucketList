5af974e12d1d77d1b9e4c81b31ce74b98f456bb7
Olivia Coffaro <email": null>
2016-11-05 12:03:07 -0400

    adding error messages for user and personality pages

--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -2,8 +2,13 @@ require 'rest-client'
 
 class PersonalitiesController < ApplicationController
   def show
-    @personality = Personality.find(params[:id])
-    @scores = @personality.parse_personality
+    if current_user
+      @personality = Personality.find(params[:id])
+      @scores = @personality.parse_personality
+    else
+      flash[:notice] = "Please log in to view this page."
+      redirect_to '/login'
+    end
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by(id: params[:id])
+    if current_user
+      @user = User.find_by(id: params[:id])
+    else
+      flash[:notice] = "Please log in to view this page."
+      redirect_to '/login'
+    end
   end
 
   def new
