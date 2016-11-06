ae524570cd02c86130e8f2b0f096d08120996de3
Derek Yang <dyy2105@columbia.edu>
2016-11-05 21:44:13 -0400

    map mb to big5 method in activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,11 @@
 class Activity < ApplicationRecord
+
+  def mb_to_bigfive(mb_type)
+    self.op = mb_type[1] == "S" ? 0.25 : 0.75
+    self.co = mb_type[3] == "J" ? 0.25 : 0.75
+    self.ex = mb_type[0] == "I" ? 0.25 : 0.75
+    self.ag = mb_type[2] == "F" ? 0.25 : 0.75
+    self.ne = 0.5
+  end
+
 end
