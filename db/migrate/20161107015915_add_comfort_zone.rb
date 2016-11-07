eb72246228da9897f3a44a11f740bf41d2e842e5
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-07 10:48:23 -0500

    Update 20161107015915_add_comfort_zone.rb

--- db/migrate/20161107015915_add_comfort_zone.rb
+++ db/migrate/20161107015915_add_comfort_zone.rb
@@ -1,6 +1,6 @@
 class AddComfortZone < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :comfortzone, :float, , :default => 0.0
+    add_column :users, :comfortzone, :float, :default => 0.0
 
   end
 end
