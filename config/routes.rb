5232905ae271899de568358a40f1f13b85519afe
Olivia Coffaro <email": null>
2016-11-07 15:03:10 -0500

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  get '/facebook' => 'users#share'
+  get '/share' => 'users#share'
 
   root 'welcome#index'
 end
