4ac398d1ecf729fddc8e0e95e88f9c8ca3ba9eb9
Olivia Coffaro <email": null>
2016-11-08 17:00:57 -0500

    session changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/login'
+      session[:user_id] = @user.id
+      redirect_to @user
     else
       flash[:notice] = "Please complete all fields."
       redirect_to '/users/new'
