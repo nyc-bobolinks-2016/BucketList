f2a517ade42af14a1a5b32b481a39260fcbc73be
Jesusmar Lima <jesusmarlima@gmail.com>
2016-11-09 18:08:47 -0500

    resolve conflicts

--- app/assets/javascripts/personality.js
+++ app/assets/javascripts/personality.js
@@ -13,13 +13,4 @@ $(document).ready(function() {
       $(".wait").toggle()
   });
 
-  $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
-
-
 });
