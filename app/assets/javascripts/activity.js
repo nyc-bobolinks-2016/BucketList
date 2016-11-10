f8499878e14d0fa8b92450b01481ced1d4f3e5f9
Derek Yang <dyy2105@columbia.edu>
2016-11-10 14:16:18 -0500

    last styling

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
-  
 
-  $(".chicken").on('click', function(){
+
+  $(".comfortzone").on('click', function(){
     var data = $(this).find("span")[1].textContent
-    debugger
   $.ajax({
     url: '/users/update',
     method: 'put',
